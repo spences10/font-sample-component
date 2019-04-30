cb6f652969303bf7e81945a8ca04de44844b6d53
Scott Spence <spences10apps@gmail.com>
2019-04-30 12:19:14 +0100

    Initial commit

---
+++ src/components/font.title.jsx
@@ -0,0 +1,8 @@
+import styled from "@emotion/styled"
+
+export const CharacterTitle = styled.p`
+  font-size: ${props => props.size || "27px"};
+  font-weight: ${props => props.fontWeight || "normal"};
+  font-style: ${props => props.fontStyle || "normal"};
+  text-transform: capitalize;
+`
