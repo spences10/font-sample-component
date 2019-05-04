1184a27c1f7814b28d0c911e74941adafbe080db
spences10 <spences10apps@gmail.com>
2019-05-04 17:27:02 +0100

    log things

--- src/components/fontList.js
+++ src/components/fontList.js
@@ -5,15 +5,11 @@ import { Dump } from './dump'
 export const Fonts = () => {
   const [fonts, setFonts] = useState(null)
   const KEY = process.env.GATSBY_FONTS_API_KEY
-  console.log('=====================')
-  console.log(KEY)
-  console.log('=====================')
   axios
     .get(
       `https://www.googleapis.com/webfonts/v1/webfonts?key=${KEY}&sort=popularity`
     )
     .then(res => {
-      console.log(res.data)
       setFonts(res.data)
     })
   return (
