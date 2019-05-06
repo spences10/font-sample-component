3f6b7f8bf7a52f5d55ad14faf11f285bdada9a13
spences10 <spences10apps@gmail.com>
2019-05-06 07:14:00 +0100

    testing

--- src/components/test.js
+++ src/components/test.js
@@ -1,34 +1,19 @@
-import styled from '@emotion/styled'
-import React from 'react'
-import { useFontsData } from '../hooks/fontsData'
+import React from 'react';
+import { useFontsData } from '../hooks/fontsData';
 
-const Select = styled.select`
-  margin: 0.25rem 0rem;
-  padding: 0rem 0rem;
-  font-family: ${({ theme }) => theme.fontBody};
-  border: 1px solid ${props => props.border};
-  box-shadow: 0px 0px 0px 1px rgba(0, 0, 0, 0.1);
-  background: ${({ theme }) => theme.foreground};
-  border-radius: 2px;
-`
-
-export const SelectOpt = styled.option`
-  font-family: ${({ theme }) => theme.fontBody};
-`
-
-export const Test = props => {
+export const Test = ({ fontFamily }) => {
   const items = useFontsData()
+
   return (
     <>
-      <Select>
-        {items.map((item, index) => {
-          return (
-            <SelectOpt placeholder="Select font..." key={index}>
-              {item.family}
-            </SelectOpt>
-          )
-        })}
-      </Select>
+      {/* {items.map((item, index) => {
+        return (
+          <div key={index}>
+            <p> {item.family} </p>
+            <Dump props={item.files} />
+          </div>
+        )
+      })} */}
     </>
   )
 }
