5ee777cf8ae7b25c46c77d106a501e59d2a34cda
spences10 <spences10apps@gmail.com>
2019-05-09 06:24:55 +0100

    styled-components  from emotion

--- src/components/font.characters.jsx
+++ src/components/font.characters.jsx
@@ -1,5 +1,5 @@
-import styled from '@emotion/styled'
 import React from 'react'
+import styled from 'styled-components'
 import { CharacterTitle } from './font.title'
 
 const Characters = `ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890¿?¡!&@‘’“”«»%*^#$£€¢/()[]{}.,®©`
